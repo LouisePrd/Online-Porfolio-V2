d6707fc52aab29e7ff7cf597f8efded3c487eaa3
LouisePrd <louise.peridy22@gmail.com>
2025-12-30 13:38:52 +0100

    ✨ Setup Next HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,66 @@
 import Image from "next/image";
+import Link from "next/link";
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div className={styles.all}>
+      <div className={styles["div-first-title"]}>
+        <h1 className={styles["first-title"]}>Creative portfolio</h1>
+      </div>
+
+      <nav className={styles.nav}>
+        <Link href="/categories" className={styles["nav-link"]}>Projects</Link>
+        <Link href="/inspirations" className={styles["nav-link"]}>Inspirations</Link>
+        <Link href="/cv" className={styles["nav-link"]}>CV</Link>
+      </nav>
+
+      <div className={styles["presentation-container"]}>
+        <div className={styles.presentation}>
+          <p className={styles.name}>Louise PERIDY,</p>
+          <p className={styles.name}>Student</p>
+          <a className={`${styles.name} ${styles.link}`} href="https://www.esiee.fr/formations/ingenieur/filieres/imac" target="_blank" rel="noopener noreferrer">
+            IMAC - ESIEE
+          </a>
+          <Image 
+            src="/imgs/props/memoji.png" 
+            alt="memoji" 
+            className={styles.memoji}
+            width={150} 
+            height={150} 
+          />
+        </div>
+
+        <div className={styles["text-presentation"]}>
+          <p>
+            Who am I? <br /><br />
+            My name is Louise PERIDY, I am 24 years old and after completing a DUT in computer science at the IUT PARIS Descartes, I spent a year at Gobelins studying for a bachelor's degree in web & mobile development, where I obtained my diploma.
+            I then enrolled in the IMAC engineering program at ESIEE Paris in September 2023 and will complete my studies in the summer of 2026.
+            <br /><br />
+            I have always been torn between an interest in computer science and a curiosity for the arts, which is why I chose to continue my studies at this school, which combines these two fields.
+            <br /><br />
+            In this portfolio, you will discover my inspirations and my various projects, which reflect my different sensibilities: enjoy your visit!
           </p>
+          <div className={styles.startdiv}>
+            <Link className={styles.btnStart} href="/categories">Start</Link>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
+
+        <div className={styles["social-media"]}>
+          <a href="https://www.linkedin.com/in/louise-peridy/" target="_blank" rel="noopener noreferrer">
+            <Image src="/imgs/socials/linkedin.png" alt="linkedin" width={30} height={30} className="social-media-img" />
           </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
+          <a href="https://github.com/LouisePrd" target="_blank" rel="noopener noreferrer">
+            <Image src="/imgs/socials/github.png" alt="github" width={30} height={30} className="social-media-img" />
+          </a>
+          <a href="mailto:louise.peridy22@gmail.com">
+            <Image src="/imgs/socials/mail.png" alt="mail" width={30} height={30} className="social-media-img" />
+          </a>
+          <a href="https://www.instagram.com/lwiz.prd/" target="_blank" rel="noopener noreferrer">
+            <Image src="/imgs/socials/insta.png" alt="instagram" width={30} height={30} className="social-media-img" />
           </a>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
